c5d251398e579ea30c80b5a5b4ba1e3352f1a7d1
NetiFebriyani <netifebriyani2223@gmail.com>
2022-08-13 01:16:42 +0700

    first commit

---
+++ app/src/main/java/com/example/notes_10119267/TentangAplikasi.java
@@ -0,0 +1,20 @@
+package com.example.notes_10119267;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TentangAplikasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_aplikasi);
+    }
+}
+
+/***
+ * NIM      :10119267
+ * Nama     :Neti Febriyani
+ * Kelas    :IF7
+ */
